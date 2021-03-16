b53a81a8b2c5dfc16a64a243423c07f7531a6ca5
padii <chudy3221@gmail.com>
2021-03-16 17:09:04 +0100

    Creation of Xamarin Project
    * Created new Xamarin AppShell project
    * Deleted useless files for the project
    * Created Login Page (not working as intended for now)
    * Added "Are you sure you want to quit?" on Login Page

---
+++ InventorySystem/InventorySystem/InventorySystem/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using InventorySystem.ViewModels;
+using InventorySystem.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace InventorySystem
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            MainTabBar.CurrentItem = LoginPage;
+        }
+    }
+}
---
+++ InventorySystem/InventorySystem/InventorySystem/Views/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using InventorySystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InventorySystem.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Are you sure ?", "Do you want to quit the application?", "Yes", "No");
+                if (result)
+                {
+                    Application.Current.Quit();
+                }
+            });   
+            return true;
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            //MainTabBar
+        }
+    }
+}
