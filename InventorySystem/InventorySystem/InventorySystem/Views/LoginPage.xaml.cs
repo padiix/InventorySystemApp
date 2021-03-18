5e2e31002fd291b8830273f1c05d581120763606
padii <chudy3221@gmail.com>
2021-03-18 12:37:14 +0100

    Fixed LoginPage
    * Border of TabBar should not be showing anymore when user is on the LoginPage

--- InventorySystem/InventorySystem/InventorySystem/Views/LoginPage.xaml.cs
+++ InventorySystem/InventorySystem/InventorySystem/Views/LoginPage.xaml.cs
@@ -16,6 +16,7 @@ namespace InventorySystem.Views
         {
             InitializeComponent();
             this.BindingContext = new LoginViewModel();
+            Shell.SetTabBarIsVisible(this, false);
         }
     }
 }
