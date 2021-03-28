4dff289e1c4062bcd577c350cc7d6969e74c1da4
padiix <chudy3221@gmail.com>
2021-03-29 00:03:01 +0200

    Improved LoginPage and some.
    * Stopped tracking .dtbcache.v2
    * Improved LoggingIn and LoggingOut
    * Unified AppShell routing into XAML file
    * Simplified GoTo's in LoginViewModel.cs
    * Changed Entries properties in LoginPage.xaml
    * Added OnTextChanged behaviour to Login and Password entries
    * Made sure that Log out is secure in terms of personal data.

--- InventorySystem/InventorySystem/InventorySystem/App.xaml.cs
+++ InventorySystem/InventorySystem/InventorySystem/App.xaml.cs
@@ -8,21 +8,37 @@ namespace InventorySystem
 {
     public partial class App : Application
     {
+        public const string EVENT_LAUNCH_MAIN_PAGE = "EVENT_LAUNCH_MAIN_PAGE";
+        public const string EVENT_LAUNCH_LOGIN_PAGE = "EVENT_LAUNCH_LOGIN_PAGE";
 
         public App()
         {
             InitializeComponent();
 
+            MessagingCenter.Subscribe<object>(this, EVENT_LAUNCH_LOGIN_PAGE, SetRootToLoginPage);
+            MessagingCenter.Subscribe<object>(this, EVENT_LAUNCH_MAIN_PAGE, SetRootToMainPage);
+
             if (Settings.RememberMe == true)
             {
+                
                 MainPage = new AppShell();
             }
             else
             {
-                MainPage = new LoginPage();
+                
+                MainPage = new NavigationPage(new LoginPage());
             }
         }
 
+        public async void SetRootToLoginPage(object sender)
+        {
+            await Shell.Current.GoToAsync("//login");
+        }
+        
+        public async void SetRootToMainPage(object sender)
+        {
+            await Shell.Current.GoToAsync("//main");
+        }
 
         protected override void OnStart()
         {
--- InventorySystem/InventorySystem/InventorySystem/ViewModels/LoginViewModel.cs
+++ InventorySystem/InventorySystem/InventorySystem/ViewModels/LoginViewModel.cs
@@ -5,14 +5,15 @@ using MvvmHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using InventorySystem.Models;
 using Xamarin.Forms;
 
 namespace InventorySystem.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public static string Login;
+        public static string Password;
 
         public Command LoginCommand { get; }
 
@@ -28,21 +29,17 @@ namespace InventorySystem.ViewModels
                 DependencyService.Get<IMessage>().ShortAlert("Uzupełnij pole \"login\" lub \"hasło\"");
                 return;
             }
-            //Need to create connectivity with API first, then we can work from there.
-            //await Xamarin.Essentials.SecureStorage.SetAsync("user-name", Login);
-            //Login = "";
-            //await Xamarin.Essentials.SecureStorage.SetAsync("password", Password);
-            //Password = "";
 
             Application.Current.MainPage = new AppShell();
+
             if (Settings.FirstRun)
             {
                 Settings.FirstRun = false;
-                await Shell.Current.GoToAsync($"//{nameof(AboutApp)}");
+                await Shell.Current.GoToAsync($"//about");
             }
             else
             {
-                await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+                await Shell.Current.GoToAsync("//main");
             }
         }
     }
