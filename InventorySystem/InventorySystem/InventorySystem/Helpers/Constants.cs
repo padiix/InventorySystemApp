2636c8a52cedc0d20311cd5502a9b98f7f93df4f
padiix <chudy3221@gmail.com>
2021-06-15 18:47:38 +0200

    New Page, icons and directory change for Constants
    * Constants.cs now are located inside helpers folder
    * New icons :
    + scanner_button50x50.png
    + icon_tools.png
    * New Page :
    + CustomScannerPage.xaml
    It's implementing a custom scanner overlay with custom behaviours

---
+++ InventorySystem/InventorySystem/InventorySystem/Helpers/Constants.cs
@@ -0,0 +1,36 @@
+﻿namespace InventorySystem
+{
+    public static class Constants
+    {
+        
+
+        //TODO: Change the "10.0.2.2" part to working address, as this one is a loopback to dev machine localhost
+        public const string AccountEndpoint = "http://10.0.2.2:5000/api/account";
+        public const string ItemsEndpoint = "http://10.0.2.2:5000/api/items";
+
+        //Endpoints for specific things
+        public const string AccountLogin = AccountEndpoint + "/login";
+        public const string AccountRegister = AccountEndpoint + "/register";
+
+        //Success messages
+        public const string DeletionSuccessful = "Pomyślnie usunięto przedmiot z bazy danych";
+        public const string UpdateSuccessful = "Pomyślnie zaktualizowano przedmiot w bazie danych";
+        public const string AddingItemSuccessful = "Pomyślnie dodano przedmiot do bazy danych";
+
+        //Errors messages
+        public const string FillInFieldsError = "Pewne pola nie są wypełnione, proszę, uzupełnij je!";
+        public const string RegistrationError = "Podczas rejestracji wystąpił błąd";
+        public const string ConnectionError = "Wystąpił błąd podczas łączenia się z API";
+        public const string ApiRejectionError = "Zgłoszenie zostało odrzucone prez API";
+        public const string ExpiredTokenError = "Token autoryzacyjny użytkownika stracił ważność";
+        public const string NoTokenError = "Brak tokena uwierzytelniającego";
+        public const string ItemError = "Wystąpił błąd podczas pobierania przedmiotu z API";
+        public const string ItemsError = "Wystąpił błąd podczas pobierania przedmiotów z API";
+        public const string SpecificItemError = "Wystąpił błąd podczas pobierania wybranego przedmiotu z API";
+        public const string EmailAndPasswordFillInError = "Uzupełnij pola \"Email\" oraz \"Hasło\" !";
+        public const string UnauthorizedError = "Błędny login lub hasło!";
+        public const string DeletionError = "Wystąpił błąd podczas usuwania przedmiotu z bazy danych";
+        public const string UpdateItemError = "Wystąpił błąd podczas zapisywania zmian przedmiotu w bazie danych";
+        public const string AddingItemError = "Wystąpił błąd podczas dodawania przedmiotu do bazy danych";
+    }
+}
---
+++ InventorySystem/InventorySystem/InventorySystem/Views/CustomScannerPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventorySystem.Models;
+using InventorySystem.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Mobile;
+
+namespace InventorySystem.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomScannerPage : ContentPage
+    {
+        private readonly RestService _restClient = new RestService();
+
+        public CustomScannerPage()
+        {
+            InitializeComponent();
+
+            zxing.AutomationId = "zxingScannerView";
+            overlay.AutomationId = "zxingDefaultOverlay";
+
+            zxing.Options = new MobileBarcodeScanningOptions()
+            {
+                DelayBetweenContinuousScans = 2000,
+                AutoRotate = false,  
+                UseFrontCameraIfAvailable = false,
+                TryHarder = true,
+                PossibleFormats = new List<ZXing.BarcodeFormat>
+                { 
+                    ZXing.BarcodeFormat.EAN_8, ZXing.BarcodeFormat.EAN_13, ZXing.BarcodeFormat.UPC_A 
+                }
+            };
+
+            overlay.ShowFlashButton = zxing.HasTorch;
+        }
+
+        private void Overlay_OnFlashButtonClicked(Button sender, EventArgs e)
+        {
+            zxing.IsTorchOn = !zxing.IsTorchOn;
+        }
+
+        private void Zxing_OnOnScanResult(Result result)
+        {
+            //Stop scanning
+            zxing.IsAnalyzing = false;
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await Shell.Current.Navigation.PopAsync();
+
+                var items = await _restClient.GetScannedItem(result.Text);
+
+                if (items != null)
+                {
+                    Dictionary<string, string> dictionaryOfChoices = new Dictionary<string, string>();
+                    List<string> choices = new List<string>();
+                    foreach (var itemInItems in items)
+                    {
+                        dictionaryOfChoices.Add($"{itemInItems.Name} - {itemInItems.DateAdded}", itemInItems.Id.ToString());
+                        choices.Add($"{itemInItems.Name} - {itemInItems.DateAdded}");
+                    }
+
+                    var userChoice = await Shell.Current.DisplayActionSheet("Wybierz przedmiot:", "Anuluj", null,
+                        choices.ToArray());
+
+                    if (userChoice.Equals("Anuluj")) return;
+
+                    dictionaryOfChoices.TryGetValue(userChoice, out var idOfChosenItem);
+
+                    var item = items.Find(match => match.Id.Equals(Guid.Parse(idOfChosenItem ?? string.Empty)));
+
+                    if (item.User.Id.Equals(Guid.Parse(StaticValues.UserId)))
+                    {
+                        await Shell.Current.GoToAsync($"item/modify?Id={item.Id}");
+
+                        return;
+                    }
+
+                    await Shell.Current.DisplayAlert("Znaleziono przedmiot",
+                        "Jednakże nie posiadasz uprawnień, aby go zmodyfikować",
+                        "Ok");
+
+                    return;
+                }
+
+                var notFoundAlertResult = await Shell.Current.DisplayAlert("Brak wyników",
+                    $"W bazie danych nie znaleziono przemiotu o kodzie: {result.Text}",
+                    "Nic nie rób",
+                    "Dodaj");
+
+                if (notFoundAlertResult) return;
+                await Shell.Current.GoToAsync($"item/add?Barcode={result.Text}");
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+    }
+}
